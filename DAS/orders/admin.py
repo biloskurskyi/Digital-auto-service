db85f1c1f0619c91db08695a9cb94f5f9eb17f65
Valerii <v.bill2003@gmail.com>
2024-02-06 14:04:32 +0200

    -added new admins tables -update order table

--- DAS/orders/admin.py
+++ DAS/orders/admin.py
@@ -7,5 +7,5 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ('client', 'car', 'start_date', 'process_status', 'info')
     search_fields = ('client__first_name', 'client__last_name', 'car__car_number')
     list_per_page = 15
-    readonly_fields = ('client', 'car',)
+    readonly_fields = ('client', 'car', 'process_status',)
 # Register your models here.
