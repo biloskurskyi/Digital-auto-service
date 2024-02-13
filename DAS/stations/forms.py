74b15d8d1905e796eb17988e4fa75dcbf5c9e84f
Valerii <v.bill2003@gmail.com>
2024-02-13 12:00:09 +0200

    Fixed: manager can only read about station!

--- DAS/stations/forms.py
+++ DAS/stations/forms.py
@@ -6,14 +6,27 @@ from .models import Station
 
 
 class CreateStationForm(forms.ModelForm):
-    # address = PlainLocationField(based_fields=['address'], zoom=7)
-
     class Meta:
         model = Station
         fields = '__all__'
         exclude = ('owner',)
 
-    # def __init__(self, *args, **kwargs):
-    #     super(CreateStationForm, self).__init__(*args, **kwargs)
-    #     self.request = kwargs.pop('request', None)
-    #     self.fields['service_station'].queryset = Station.get_owned_stations(self.request.user)
+    def __init__(self, *args, **kwargs):
+        owner = kwargs.pop('owner', None)
+        username = kwargs.pop('username', None)
+        super(CreateStationForm, self).__init__(*args, **kwargs)
+        if owner:
+            self.owner = owner
+        else:
+            self.owner = None
+        # print(owner, username)
+        # print(owner == username)
+        if owner != username:
+            print('ok')
+            self.fields['name'].disabled = True
+            self.fields['address'].disabled = True
+
+# def __init__(self, *args, **kwargs):
+#     super(CreateStationForm, self).__init__(*args, **kwargs)
+#     self.request = kwargs.pop('request', None)
+#     self.fields['service_station'].queryset = Station.get_owned_stations(self.request.user)
