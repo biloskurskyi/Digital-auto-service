23f399d505dd1661569595398a5421c31aa48afa
Valerii <v.bill2003@gmail.com>
2024-03-05 21:19:29 +0200

    post for new acc create active user. fix it

--- DAS/accounts/serializer.py
+++ DAS/accounts/serializer.py
@@ -1,9 +1,25 @@
 from rest_framework import fields, serializers
+from rest_framework.exceptions import ValidationError
 
 from accounts.models import AccountUsers
 
 
 class AccountSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+
     class Meta:
         model = AccountUsers
-        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'phone_number', 'is_active')
+        fields = ('username', 'first_name', 'last_name', 'email', 'phone_number', 'is_active', 'password')
+
+    def validate_email(self, value):
+        if AccountUsers.objects.filter(email=value, is_active=True, is_verified_email=True).exists():
+            raise serializers.ValidationError("This email address is already used!")
+        return value
+
+    def create(self, validated_data):
+
+        password = validated_data.pop('password')
+        user = AccountUsers.objects.create(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
--- DAS/api/views.py
+++ DAS/api/views.py
@@ -1,10 +1,14 @@
+from datetime import timedelta
+import uuid
 from django.shortcuts import render
-from requests import Response
+from django.utils.timezone import now
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
-from accounts.models import AccountUsers
+from accounts.models import AccountUsers, EmailVerification
 from accounts.serializer import AccountSerializer
 
 
@@ -16,6 +20,8 @@ class AccountViewSet(ModelViewSet):
     def get_permissions(self):
         if self.action in ('create', 'update', 'partial_update'):
             self.permission_classes = (IsAdminUser,)
+        if self.action in ('create',):
+            return []
         return super(AccountViewSet, self).get_permissions()
 
     def get_queryset(self):
@@ -23,3 +29,19 @@ class AccountViewSet(ModelViewSet):
         if self.request.method == 'GET':  # and self.request.user.owner is None
             queryset = AccountUsers.objects.filter(owner=self.request.user)
         return queryset
+
+    def create(self, request, *args, **kwargs):
+        request.data['is_active'] = False
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.save(is_active=False)
+        expiration = now() + timedelta(hours=24)
+        record = EmailVerification.objects.create(code=uuid.uuid4(), user=user, expiration=expiration)
+        record.send_verification_email(
+            serializer.validated_data['password'])
+        user.is_active = False  # Встановлюємо is_active на False перед збереженням користувача
+        # for i in user:
+        #     print(i)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
